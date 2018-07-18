9760d2f367e79990ee32fb20f3c63cad6ec585e5
Noah Stolk <noah.stolk1997@gmail.com>
2018-07-18 20:50:34 +0200

    no message

--- ProjectEuler/Problems.cs
+++ ProjectEuler/Problems.cs
@@ -62,7 +62,7 @@ namespace ProjectEuler
 				for (int j = 100; j < 1000; j++)
 				{
 					x = i * j;
-					
+
 					if (x.ToString() == x.ToString().Reverse())
 						if (x > largest)
 							largest = x;
@@ -202,6 +202,63 @@ namespace ProjectEuler
 			}
 
 			return total;
+		}
+
+		public static int Problem18()
+		{
+			const int triangleSize = 15;
+			
+			int[][] matrix =
+			{
+				new int[] { 75 },
+				new int[] { 95, 64 },
+				new int[] { 17, 47, 82 },
+				new int[] { 18, 35, 87, 10 },
+				new int[] { 20, 4, 82, 47, 65 },
+				new int[] { 19, 1, 23, 75, 3, 34 },
+				new int[] { 88, 2, 77, 73, 7, 63, 67 },
+				new int[] { 99, 65, 4, 28, 6, 16, 70, 92 },
+				new int[] { 41, 41, 26, 56, 83, 40, 80, 70, 33 },
+				new int[] { 41, 48, 72, 33, 47, 32, 37, 16, 94, 29 },
+				new int[] { 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14 },
+				new int[] { 70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57 },
+				new int[] { 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48 },
+				new int[] { 63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31 },
+				new int[] { 4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23 }
+			};
+
+			for (int y = triangleSize - 2; y > -1; y--)
+			{
+				for (int x = 0; x < y + 1; x++)
+				{
+					if (matrix[y + 1][x] < matrix[y + 1][x + 1])
+						matrix[y][x] += matrix[y + 1][x + 1];
+					else
+						matrix[y][x] += matrix[y + 1][x];
+				}
+			}
+
+			return matrix[0][0];
+		}
+
+		public static int Problem37()
+		{
+			int x = 1;
+			int f = 0;
+			int t = 0;
+
+			while (f < 11)
+			{
+				int y = x;
+				if (MathUtils.IsTruncatablePrime(x))
+				{
+					f++;
+					t += x;
+				}
+				x += 2;
+			}
+
+			return t;
 		}
 
 		public static int Problem179(int limit)
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+using XtendedNET.Extensions;
 
 namespace ProjectEuler
 {
@@ -13,39 +16,49 @@ namespace ProjectEuler
 
 			for (; ; )
 			{
-				WriteColor("Enter a problem to solve: ", ConsoleColor.Yellow);
-				string problem = Console.ReadLine();
+				WriteColor("Enter problems to solve (separated by commas): ", ConsoleColor.Yellow);
+				string[] problems = Console.ReadLine().Split(',').Distinct().ToArray();
 
 				Type type = typeof(Problems);
-				MethodInfo method = type.GetMethod($"Problem{problem}");
-				Problems problems = new Problems();
+				Dictionary<string, MethodInfo> methods = new Dictionary<string, MethodInfo>();
+				for (int i = 0; i < problems.Length; i++)
+					methods.Add(problems[i], type.GetMethod($"Problem{problems[i]}"));
+				Problems p = new Problems();
 
 				Console.WriteLine();
+				WriteLineColor(string.Format("{0, -10}{1, -15}{2, -15}", "Problem", "Answer", "Time"), ConsoleColor.Magenta);
+				Console.WriteLine();
 
-				try
+				foreach (KeyValuePair<string, MethodInfo> kvp in methods)
 				{
-					Stopwatch stopwatch = new Stopwatch();
-					stopwatch.Start();
-					WriteLineColor(method.Invoke(problems, null), ConsoleColor.White);
-					stopwatch.Stop();
-					WriteLineColor($"Execution took: {stopwatch.Elapsed}", ConsoleColor.Cyan);
-				}
-				catch (Exception)
-				{
-					WriteLineColor("Problem not found.", ConsoleColor.Red);
+					try
+					{
+						Stopwatch stopwatch = new Stopwatch();
+						stopwatch.Start();
+						WriteLineColor(string.Format("{0, -10}{1, -15}{2, -15}", kvp.Value.Name.Numeric(), kvp.Value.Invoke(p, null), stopwatch.Elapsed), ConsoleColor.White);
+					}
+					catch (NullReferenceException)
+					{
+						WriteLineColor(string.Format("{0, -10}Problem not found.", kvp.Key), ConsoleColor.Red);
+					}
+					catch (Exception ex)
+					{
+						WriteLineColor("FATAL ERROR", ConsoleColor.Red);
+						WriteLineColor(ex.Message, ConsoleColor.Red);
+					}
 				}
 
 				Console.WriteLine();
 			}
 		}
 
-		public static void WriteLineColor(object message, ConsoleColor color)
+		public static void WriteLineColor(string message, ConsoleColor color)
 		{
 			Console.ForegroundColor = color;
 			Console.WriteLine(message);
 		}
 
-		public static void WriteColor(object message, ConsoleColor color)
+		public static void WriteColor(string message, ConsoleColor color)
 		{
 			Console.ForegroundColor = color;
 			Console.Write(message);
